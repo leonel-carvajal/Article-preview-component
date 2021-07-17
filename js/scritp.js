78be933539001caab9f6363327fdf468929a773a
Leonel Carvajal <liosoft777@gmail.com>
2021-07-16 19:30:24 -0600

    Design desktop v0.1

--- js/scritp.js
+++ js/scritp.js
@@ -1,6 +1,6 @@
 const Share = document.querySelector('.footer__share')
 const ContainerShare  = document.querySelector('.share')
-const ShareClose = document.querySelector('.share__icon')
+const ShareClose = document.querySelector('.share')
 
 Share.addEventListener('click',()=>ContainerShare.classList.add('share--show'));
 ShareClose.addEventListener('click',()=>ContainerShare.classList.remove('share--show'))
